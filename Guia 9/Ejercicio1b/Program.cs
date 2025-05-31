9496950c28b3c7c75a072e04238bf31e134d987b
FacuJesus <facundojesus10@hotmail.com>
2025-05-31 09:06:29 -0300

    Fibonacci Rercursivo

---
+++ Guia 9/Ejercicio1b/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ejercicio1b
+{
+    internal class Program
+    {
+        static int CalcularTerminoFibonacci(int termino)
+        {
+            switch(termino)
+            {
+                case 0:
+                case 1:
+                    return termino;                 
+                default:
+                    int valor = CalcularTerminoFibonacci(termino - 1) + CalcularTerminoFibonacci(termino - 2); 
+                    return valor;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la cantidad de términos para calcular la Seria de Fibonacci:");
+            int terminos = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < terminos; i++) {
+                int valor = CalcularTerminoFibonacci(i);
+                Console.WriteLine(valor);
+            }
+        }
+    }
+}
